f8644a1589b56cdecd8a876cf0d783446fd7bcc8
huahao <lvhuahao@ad61v1.com>
2020-03-17 09:39:03 +0800

    init first commit

---
+++ meta.js
@@ -0,0 +1,46 @@
+moudle.exports = {
+  helpers: {
+    if_or(v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this);
+      }
+      return options.inverse(this);
+    }
+  },
+  prompts: {
+    name: {
+      when: "isNotTest",
+      type: "string",
+      required: true,
+      message: "项目名称"
+    },
+    description: {
+      when: "isNotTest",
+      type: "string",
+      required: false,
+      message: "项目描述",
+      default: "a React +typescript project"
+    },
+    version: {
+      when: 'isNotTest',
+      type: 'string',
+      default: '1.0.0'
+    },
+    author: {
+      when: "isNotTest",
+      type: "string",
+      message: "作者"
+    },
+    router: {
+      when: "isNotTest",
+      type: "confirm",
+      message: "安装react-router?"
+    },
+    redux: {
+      when: 'isNotTest',
+      type: 'confirm',
+      message: '安装redux?'
+    },
+    completeMessage: "安装成功"
+  }
+};
---
+++ template/build/webpack.dll.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const DllPlugin = require("webpack/lib/DllPlugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+    name: "production",
+    entry: {
+        framework: ["react", "redux", "react-dom", "react-redux"]
+    },
+    output: {
+        filename: "[name].dll.js",
+        path: path.resolve(__dirname, "../dll"),
+        library: "_dll_[name]"
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new DllPlugin({
+            name: "_dll_[name]",
+            path: path.resolve(__dirname, "../dll", "[name].manifest.json")
+        })
+
+    ]
+};
